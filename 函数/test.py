b98f2e02f5be05b8789e53213bf1c78e43a1b76b
pro <w1914563332@gmail.com>
2025-10-27 23:36:58 +0800

    1

--- 函数/test.py
+++ 函数/test.py
@@ -1,27 +1,11 @@
-from functools import wraps
-from idlelib.debugobj import myrepr
+def func(start,stop,step):
+    while start < stop:
+        yield start
+        start += step
 
+start = func(1, 5, 3)
+for i in start:
+    print(i)
 
-def file(engine):
-    def auth(func):
-        def wapper(*args,**kwargs):
-            username=input("input username").strip()
-            password=input("input pw").strip()
-            if engine=="mysql":
-                if username=="wsa" and password=="123":
-                    res=func()
-                    return res
-                else:
-                    print("error")
-            else:
-                print("error")
-
-        return wapper
-    return auth
-
-
-@file('mysql') # index=auth的内存地址(index)
-def index():
-    print("from index")
-
-print(index)
+print(type(start))
+print(type(func))
