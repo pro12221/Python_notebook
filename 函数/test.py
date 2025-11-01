9a86736fade498b56b0ae16572de8593b7c334b2
pro <w1914563332@gmail.com>
2025-11-01 23:16:04 +0800

    1

--- 函数/test.py
+++ 函数/test.py
@@ -1,10 +1,25 @@
-l = [1, [2, [3, [4]]]]
+nums = [-5, 3, 7, 19, 23, 45, 63, 67, 103]
+# def search(find_num,列表):
+#     mid_val = 找到列表的中间值
+#     if find_num > mid_val:
+#         # 接下来应该查找的是列表的右半部分
+#         新列表 = 切出列表的右半部分
+#         search(find_num,新列表)
+#     elif find_num < mid_val:
+#         # 接下来应该查找的是列表的左半部分
+#         新列表 = 切出列表的左半部分
+#         search(find_num, 新列表)
+#     else:
+#         print('find it')
+def search(find_num,nums_list):
+    mid_index = len(nums_list) // 2
+    mid_val=nums_list[mid_index]
+    if mid_val < find_num:
+        search(find_num,nums_list[mid_index+1:])
+    elif mid_val > find_num:
+        search(find_num,nums_list[:mid_index])
+    else:
+        print(find_num)
 
-def f(lst):  # 添加参数
-    for item in lst:
-        if type(item) is list:
-            f(item)  # 递归调用时传递参数
-        else:
-            print(item)
 
-f(l)  # 调用时传递参数
+search(19,nums)
